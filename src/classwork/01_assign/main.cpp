fddad6eab207cf76ba5e34b5b576001c7d3d4b98
taplinseth <seth.taplin@g.austincc.edu>
2021-02-05 19:25:27 -0600

    Assignment 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,12 +1,24 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
-
+#include <iostream>
+using std::cin;		
+using std::cout;
 
 int main() 
 {
+	int num;
+	cout<<"Enter number: ";
+	cin>>num;
 
-	return 0;
+	int result;
+	result = multiply_numbers(num);
+	cout<<"Result: "<<result<<"\n";
+
+	int num1 = 4;
+	result = multiply_numbers(num1);
+	cout<<"Result: "<<result<<"\n";
+
+	return result;
 }
 
--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -7,6 +7,12 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+//Multiplies num1 and num2
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
+	return num1 * num2;
+}
 
 
 
